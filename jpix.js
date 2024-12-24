079c801e622610b2403f3c266dcb4f6221a64879
huaethan <ethanhua7378@gmail.com>
2024-12-23 20:26:33 -0500

    tesseract.js being annoying

--- jpix.js
+++ jpix.js
@@ -2,7 +2,7 @@ import { createWorker } from 'tesseract.js';
 
 (async () => {
     const worker = await createWorker('jpn');
-    const ret = await worker.recognize('C:\\Users\\ethan\\Pictures\\Screenshots\\gomen.png');
+    const ret = await worker.recognize('C:\\Users\\ethan\\Pictures\\Screenshots\\test.png');
     console.log(ret.data.text);
     await worker.terminate();
 })();
