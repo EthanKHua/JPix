752d72564e03e5448619510efe712c94e22cf47c
huaethan <ethanhua7378@gmail.com>
2024-12-25 23:21:35 -0500

    key/mouse events are working as intended

--- jpix.js
+++ jpix.js
@@ -1,36 +1,64 @@
+const rect = document.createElement("div");
+rect.style.position = "absolute";
+rect.style.backgroundColor = "rgba(0,0,0,0)"; // transparent
+rect.style.border = "1.5px dashed red";
+document.body.appendChild(rect);
+
+const activation_key = "KeyQ";
+const ctrl = false;
+const alt = false;
+const shift = false;
+
+let keyPressed = false;
+let x1 = 0, y1 = 0;
+let x2 = 0, y2 = 0;
+
+window.addEventListener("keydown", (e) => {
+    if(keyPressed || e.code !== activation_key) return;
+    keyPressed = true;
+    x1 = x2; y1 = y2;
+    update_rectangle();
+});
+
+window.addEventListener("mousemove", (e) => {
+    x2 = e.pageX; y2 = e.pageY;
+    if(!keyPressed) return;
+    update_rectangle();
+});
+
+window.addEventListener("keyup", (e) => {
+    if(!keyPressed || e.code !== activation_key) return;
+    keyPressed = false;
+    recognize(x1, y1, x2, y2);
+    reset_rectangle();
+});
+
+function update_rectangle() {
+    let x0 = Math.min(x1, x2);
+    let y0 = Math.min(y1, y2);
+    let width = Math.abs(x1 - x2);
+    let height = Math.abs(y1 - y2);
+    rect.style.left = x0 + "px";
+    rect.style.top = y0 + "px";
+    rect.style.width = width + "px";
+    rect.style.height = height + "px";
+}
+
+function reset_rectangle() {
+    rect.style.top = "0px";
+    rect.style.left = "0px";
+    rect.style.width = "0px";
+    rect.style.height = "0px";
+}
+
 function recognize(x1, y1, x2, y2) {
     fetch(`http://localhost:5000/foo?x=${x2}&y=${y2}`)
         .then(response => response.json())
         .then(data => {
             document.title = data.result;
-            console.log(data.result);
         })
         .catch(error => {console.log(error);});
 }
-
-let mousePressed = false;
-let x1 = 0, y1 = 0;
-let x2 = 0, y2 = 0;
-
-window.addEventListener("mousedown", (e) => {
-    if(mousePressed) return;
-    mousePressed = true;
-    x1 = e.clientX; y1 = e.clientY + document.scrollingElement.scrollTop;
-    x2 = x1; y2 = y1;
-    
-});
-
-window.addEventListener("mousemove", (e) => {
-    if(!mousePressed) return;
-    // document.title = e.clientX.toString() + ", " + e.clientY.toString();
-});
-
-window.addEventListener("mouseup", (e) => {
-    if(!mousePressed) return;
-    mousePressed = false;
-    x2 = e.clientX; y2 = e.clientY + document.scrollingElement.scrollTop;
-    recognize(x1, y1, x2, y2);
-});
 
 function get_illust_id() {
     const re = /artworks\/\d*$/;
